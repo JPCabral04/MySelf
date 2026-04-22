61883edcf36037cff370caceeb6cd5c3d4abc7f6
jp <jpqcabral04@gmail.com>
2026-04-22 19:43:19 -0300

    fix(user): correct route path for fetching user by email

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -6,7 +6,7 @@ export async function userRoutes(fastify: FastifyInstance) {
   fastify.post('/', userController.post);
   fastify.get('/', userController.get);
   fastify.get('/:id', userController.getById);
-  fastify.get('/:email', userController.getByEmail);
+  fastify.get('/email/:email', userController.getByEmail);
   fastify.put('/:id', userController.put);
   fastify.delete('/:id', userController.delete);
 }
