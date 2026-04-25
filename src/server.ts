c60dbf65218deaf2e7bd06b755fe6dbcae175e37
jp <jpqcabral04@gmail.com>
2026-04-25 18:10:41 -0300

    feat(server): register additional routes for agenda items, tasks, events, financial items, transactions, goals, investments, activities, habit modules, and daily records

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,16 @@
 import Fastify from 'fastify'
 import { userRoutes } from './routers/user.router'
 import { categoryRoutes } from './routers/category.router'
+import { agendaItemRoutes } from './routers/agendaItem.router'
+import { taskRoutes } from './routers/task.router'
+import { eventRoutes } from './routers/event.router'
+import { financialItemRoutes } from './routers/financialItem.router'
+import { transactionRoutes } from './routers/transaction.router'
+import { goalRoutes } from './routers/goal.router'
+import { investmentRoutes } from './routers/investment.router'
+import { activityRoutes } from './routers/activity.router'
+import { habitModuleRoutes } from './routers/habitModule.router'
+import { dailyRecordRoutes } from './routers/dailyRecord.router'
 
 const fastify = Fastify({
   logger: true
@@ -13,6 +23,16 @@ fastify.get('/', async (request, reply) => {
 
 fastify.register(userRoutes, { prefix: '/users' })
 fastify.register(categoryRoutes, { prefix: '/categories' })
+fastify.register(agendaItemRoutes, { prefix: '/agenda-items' })
+fastify.register(taskRoutes, { prefix: '/tasks' })
+fastify.register(eventRoutes, { prefix: '/events' })
+fastify.register(financialItemRoutes, { prefix: '/financial-items' })
+fastify.register(transactionRoutes, { prefix: '/transactions' })
+fastify.register(goalRoutes, { prefix: '/goals' })
+fastify.register(investmentRoutes, { prefix: '/investments' })
+fastify.register(activityRoutes, { prefix: '/activities' })
+fastify.register(habitModuleRoutes, { prefix: '/habit-modules' })
+fastify.register(dailyRecordRoutes, { prefix: '/daily-records' })
 
 /**
  * Run the server!
